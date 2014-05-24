adaa082297ca1beba70f17da43ff8b7aac0bc089
deaddog <mikkelslsl@gmail.com>
2014-05-24 15:18:53 +0200

    The non-generic PDFGroup inherits the generic version.

--- Objects/PDFGroup.cs
+++ Objects/PDFGroup.cs
@@ -10,18 +10,15 @@ namespace DeadDog.PDF
     /// Groups are collections of <see cref="PDFObject"/> objects
     /// In a <see cref="PDFGroup"/> objects are collected in the protected list property.
     /// </summary>
-    public abstract class PDFGroup : IPDFGroup
+    public abstract class PDFGroup : PDFGroup<PDFObject>
     {
-        private PointF offset;
-        private List<PDFObject> privatelist;
-
         /// <summary>
         /// Initializes a new instance of the <see cref="PDFGroup"/> class.
         /// </summary>
         /// <param name="offsetX">The x offset for this object.</param>
         /// <param name="offsetY">The y offset for this object.</param>
         public PDFGroup(float offsetX, float offsetY)
-            : this(new PointF(offsetX, offsetY))
+            : base(offsetX, offsetY)
         {
         }
         /// <summary>
@@ -29,74 +26,9 @@ namespace DeadDog.PDF
         /// </summary>
         /// <param name="offset">The offset for this object.</param>
         public PDFGroup(PointF offset)
+            : base(offset)
         {
-            this.offset = offset;
-            this.privatelist = new List<PDFObject>();
         }
-
-        #region IPDFGroup Members
-
-        /// <summary>
-        /// Returns the location of any element within this <see cref="PDFGroup"/>.
-        /// </summary>
-        /// <param name="obj">The element whichs location is returned.</param>
-        /// <returns>The location of obj.</returns>
-        public abstract System.Drawing.PointF GetLocation(PDFObject obj);
-
-        #endregion
-
-        #region IPDFObject Members
-
-        /// <summary>
-        /// Gets or sets the offset for this <see cref="PDFGroup"/>.
-        /// </summary>
-        public PointF Offset
-        {
-            get { return offset; }
-            set { offset = value; }
-        }
-        /// <summary>
-        /// Gets or sets the x offset for this <see cref="PDFGroup"/>.
-        /// </summary>
-        public float OffsetX
-        {
-            get { return offset.X; }
-            set { offset.X = value; }
-        }
-        /// <summary>
-        /// Gets or sets the y offset for this <see cref="PDFGroup"/>.
-        /// </summary>
-        public float OffsetY
-        {
-            get { return offset.Y; }
-            set { offset.Y = value; }
-        }
-
-        /// <summary>
-        /// Gets the width of this <see cref="PDFGroup"/>.
-        /// </summary>
-        public abstract float Width
-        {
-            get;
-        }
-        /// <summary>
-        /// Gets the height of this <see cref="PDFGroup"/>.
-        /// </summary>
-        public abstract float Height
-        {
-            get;
-        }
-
-        /// <summary>
-        /// Collects all pdf objects contained within this group.
-        /// </summary>
-        /// <param name="collector">An <see cref="ObjectCollector"/> to which all pdf objects are added.</param>
-        public void Collect(ObjectCollector collector)
-        {
-            collector.Add(privatelist);
-        }
-
-        #endregion
     }
 
     /// <summary>
