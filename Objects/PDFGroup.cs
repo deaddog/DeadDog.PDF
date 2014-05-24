ad4190d268ae3ded9021b7c406aecaf29455ba60
deaddog <mikkelslsl@gmail.com>
2014-05-24 15:26:12 +0200

    PDFGroup inherits PDFObject and its location/size handling.

--- Objects/PDFGroup.cs
+++ Objects/PDFGroup.cs
@@ -17,16 +17,16 @@ namespace DeadDog.PDF
         /// </summary>
         /// <param name="offsetX">The x offset for this object.</param>
         /// <param name="offsetY">The y offset for this object.</param>
-        public PDFGroup(float offsetX, float offsetY)
-            : base(offsetX, offsetY)
+        public PDFGroup(bool canResize)
+            : base(canResize)
         {
         }
         /// <summary>
         /// Initializes a new instance of the <see cref="PDFGroup"/> class.
         /// </summary>
         /// <param name="offset">The offset for this object.</param>
-        public PDFGroup(PointF offset)
-            : base(offset)
+        public PDFGroup(bool canResize, RectangleF rectangle)
+            : base(canResize, rectangle)
         {
         }
     }
@@ -37,9 +37,8 @@ namespace DeadDog.PDF
     /// In a <see cref="PDFGroup{T}"/> objects are collected in the protected list property.
     /// </summary>
     /// <typeparam name="T">The type of elements in the pdf group</typeparam>
-    public abstract class PDFGroup<T> : IPDFGroup<T> where T : PDFObject
+    public abstract class PDFGroup<T> : PDFObject, IPDFGroup<T> where T : PDFObject
     {
-        private PointF offset;
         private List<T> privatelist;
 
         /// <summary>
@@ -47,22 +46,33 @@ namespace DeadDog.PDF
         /// </summary>
         /// <param name="offsetX">The x offset for this object.</param>
         /// <param name="offsetY">The y offset for this object.</param>
-        public PDFGroup(float offsetX, float offsetY)
-            : this(new PointF(offsetX, offsetY))
+        public PDFGroup(bool canResize)
+            : base(canResize, RectangleF.Empty)
         {
+            this.privatelist = new List<T>();
         }
         /// <summary>
         /// Initializes a new instance of the <see cref="PDFGroup{T}"/> class.
         /// </summary>
         /// <param name="offset">The offset for this object.</param>
-        public PDFGroup(PointF offset)
+        public PDFGroup(bool canResize, RectangleF rectangle)
+            : base(canResize, rectangle)
         {
-            this.offset = offset;
             this.privatelist = new List<T>();
         }
         public List<T> Objects
         {
             get { return privatelist; }
+        }
+
+        /// <summary>
+        /// Collects all pdf objects contained within this group.
+        /// </summary>
+        /// <param name="collector">An <see cref="ObjectCollector"/> to which all pdf objects are added.</param>
+        public void Collect(ObjectCollector collector)
+        {
+            foreach (T t in privatelist)
+                collector.Add(t);
         }
 
         #region IPDFGroup Members
@@ -78,60 +88,6 @@ namespace DeadDog.PDF
                 return GetLocation((T)obj);
             else
                 throw new ArgumentException("obj is not of type " + typeof(T).ToString() + ".", "obj");
-        }
-
-        #endregion
-
-        #region IPDFObject Members
-
-        /// <summary>
-        /// Gets or sets the offset for this <see cref="PDFGroup{T}"/>.
-        /// </summary>
-        public PointF Offset
-        {
-            get { return offset; }
-            set { offset = value; }
-        }
-        /// <summary>
-        /// Gets or sets the x offset for this <see cref="PDFGroup{T}"/>.
-        /// </summary>
-        public float OffsetX
-        {
-            get { return offset.X; }
-            set { offset.X = value; }
-        }
-        /// <summary>
-        /// Gets or sets the y offset for this <see cref="PDFGroup{T}"/>.
-        /// </summary>
-        public float OffsetY
-        {
-            get { return offset.Y; }
-            set { offset.Y = value; }
-        }
-
-        /// <summary>
-        /// Gets the width of this <see cref="PDFGroup{T}"/>.
-        /// </summary>
-        public abstract float Width
-        {
-            get;
-        }
-        /// <summary>
-        /// Gets the height of this <see cref="PDFGroup{T}"/>.
-        /// </summary>
-        public abstract float Height
-        {
-            get;
-        }
-
-        /// <summary>
-        /// Collects all pdf objects contained within this group.
-        /// </summary>
-        /// <param name="collector">An <see cref="ObjectCollector"/> to which all pdf objects are added.</param>
-        public void Collect(ObjectCollector collector)
-        {
-            foreach (T t in privatelist)
-                collector.Add(t);
         }
 
         #endregion
