2171376bfd57cfb2997f306c4a67aef13ccf08e1
deaddog <mikkelslsl@gmail.com>
2014-05-24 12:24:18 +0200

    PDFObject supports resizing if the CanResize property returns true.

--- Objects/PDFObject.cs
+++ Objects/PDFObject.cs
@@ -8,27 +8,27 @@ namespace DeadDog.PDF
     /// <summary>
     /// Implements basic functionalities for pdf objects.
     /// </summary>
-    public abstract class PDFObject : IPDFObject
+    public abstract class PDFObject
     {
-        private PointF offset;
+        private bool canResize;
+        private RectangleF rectangle;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="PDFObject" /> class.
+        /// Initializes a new instance of the <see cref="PDFObject" /> class with offset (0,0) and size (0,0).
         /// </summary>
-        /// <param name="offsetX">The x offset for this object.</param>
-        /// <param name="offsetY">The y offset for this object.</param>
-        public PDFObject(float offsetX, float offsetY)
-            : this(new PointF(offsetX, offsetY))
+        public PDFObject(bool canResize)
+            : this(canResize, RectangleF.Empty)
         {
         }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="PDFObject"/> class.
         /// </summary>
-        /// <param name="offset">The offset for this object.</param>
-        public PDFObject(PointF offset)
+        /// <param name="rectangle">The offset and size of this object.</param>
+        public PDFObject(bool canResize, RectangleF rectangle)
         {
-            this.offset = offset;
+            this.canResize = canResize;
+            this.rectangle = rectangle;
         }
 
         /// <summary>
@@ -57,18 +57,67 @@ namespace DeadDog.PDF
         }
 
         /// <summary>
-        /// Gets the width of this <see cref="PDFObject"/>.
+        /// Gets a value indicating whether this instance can resize.
         /// </summary>
-        public abstract float Width
+        /// <value>
+        /// <c>true</c> if this instance can resize; otherwise, <c>false</c>.
+        /// </value>
+        public bool CanResize
         {
-            get;
+            get { return canResize; }
+        }
+
+        /// <summary>
+        /// Gets or sets the size of this <see cref="PDFObject"/>.
+        /// </summary>
+        public SizeF Size
+        {
+            get { return canResize ? rectangle.Size : getSize(); }
+            set
+            {
+                if (!canResize)
+                    throw new InvalidOperationException(typeof(PDFObject).Name + " cannot be resized when CanResize is false.");
+                else
+                    rectangle.Size = value;
+            }
         }
         /// <summary>
-        /// Gets the height of this <see cref="PDFObject"/>.
+        /// Gets or sets the width of this <see cref="PDFObject"/>.
         /// </summary>
-        public abstract float Height
+        public float Width
         {
-            get;
+            get { return canResize ? rectangle.Width : getSize().Width; }
+            set
+            {
+                if (!canResize)
+                    throw new InvalidOperationException(typeof(PDFObject).Name + " cannot be resized when CanResize is false.");
+                else
+                    rectangle.Width = value;
+            }
+        }
+        /// <summary>
+        /// Gets or sets the height of this <see cref="PDFObject"/>.
+        /// </summary>
+        public float Height
+        {
+            get { return canResize ? rectangle.Height : getSize().Height; }
+            set
+            {
+                if (!canResize)
+                    throw new InvalidOperationException(typeof(PDFObject).Name + " cannot be resized when CanResize is false.");
+                else
+                    rectangle.Height = value;
+            }
+        }
+
+        /// <summary>
+        /// When implemented in a derived class, gets the size of the <see cref="PDFObject"/>.
+        /// This method will only be called when <see cref="CanResize"/> is <c>false</c>.
+        /// </summary>
+        /// <returns>The size of the <see cref="PDFObject"/>.</returns>
+        protected virtual SizeF getSize()
+        {
+            return rectangle.Size;
         }
 
         /// <summary>
