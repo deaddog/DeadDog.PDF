9ef8eda912716bd7a4fa3b239250b6b3af523f32
deaddog <mikkelslsl@gmail.com>
2014-05-24 12:40:40 +0200

    Fixed the constructors for the Box object to only be concerned with the size of the box.

--- Objects/Leafs/Box.cs
+++ Objects/Leafs/Box.cs
@@ -8,88 +8,59 @@ namespace DeadDog.PDF
     /// <summary>
     /// Holds information required to draw a box in a pdf document.
     /// </summary>
-    public class Box : PDFObjectSizeable, IPDFObject
+    public class Box : PDFObject, IPDFObject
     {
         private Color fill;
         private Color border;
-        private SizeF size;
         private float width = 0.5f;
 
         private bool hasBorder = true, hasFill = true;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Box"/> class with the selected properties.
+        /// Initializes a new instance of the <see cref="Box"/> class.
         /// </summary>
-        /// <param name="width">The width of this box.</param>
-        /// <param name="height">The height of this box.</param>
-        /// <param name="visible">true if both border and fill of this box is displayed. false is not.</param>
-        public Box(float width, float height, bool visible)
-            : this(new RectangleF(0, 0, width, height), visible)
+        /// <param name="width">The width of the box.</param>
+        /// <param name="height">The height of the box.</param>
+        public Box(float width, float height)
+            : this(0, 0, width, height)
         {
-        }
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Box"/> class with the selected properties.
-        /// </summary>
-        /// <param name="rectangle">The location and size of this box.</param>
-        /// <param name="visible">true if both border and fill of this box is displayed. false is not.</param>
-        public Box(RectangleF rectangle, bool visible)
-            : this(rectangle, Color.White, Color.Black, 0.5f)
-        {
-            hasBorder = hasFill = visible;
-        }
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Box"/> class with the selected properties.
-        /// </summary>
-        /// <param name="width">The width of this box.</param>
-        /// <param name="height">The height of this box.</param>
-        /// <param name="fill">The color used for filling this box.</param>
-        /// <param name="border">The color used for drawing the border of this box.</param>
-        /// <param name="borderWidth">The width (thickness) of the border of this box.</param>
-        public Box(float width, float height, Color fill, Color border, float borderWidth)
-            : this(new RectangleF(0, 0, width, height), fill, border, borderWidth)
-        {
-        }
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Box"/> class with the selected properties.
-        /// </summary>
-        /// <param name="rectangle">The location and size of this box.</param>
-        /// <param name="fill">The color used for filling this box.</param>
-        /// <param name="border">The color used for drawing the border of this box.</param>
-        /// <param name="borderWidth">The width (thickness) of the border of this box.</param>
-        public Box(RectangleF rectangle, Color fill, Color border, float borderWidth)
-            : base()
-        {
-            this.Location = rectangle.Location;
-            this.size = rectangle.Size;
-            this.fill = fill;
-            this.border = border;
-            this.width = borderWidth;
         }
 
         /// <summary>
-        /// Gets or sets the size of this <see cref="Box"/> object.
+        /// Initializes a new instance of the <see cref="Box"/> class.
         /// </summary>
-        public override SizeF Size
+        /// <param name="size">The size of the box.</param>
+        public Box(SizeF size)
+            : this(new RectangleF(PointF.Empty, size))
         {
-            get { return size; }
-            set { size = value; }
         }
 
         /// <summary>
-        /// Gets or sets the height of this <see cref="Box"/> object.
+        /// Initializes a new instance of the <see cref="Box"/> class.
         /// </summary>
-        public override float Height
+        /// <param name="offsetX">The x offset for the box.</param>
+        /// <param name="offsetY">The y offset for the box.</param>
+        /// <param name="width">The width of the box.</param>
+        /// <param name="height">The height of the box.</param>
+        public Box(float offsetX, float offsetY, float width, float height)
+            : this(RectangleF.FromLTRB(offsetX, offsetY, offsetX + width, offsetY + height))
         {
-            get { return size.Height; }
-            set { size.Height = value; }
         }
+
         /// <summary>
-        /// Gets or sets the width of this <see cref="Box"/> object.
+        /// Initializes a new instance of the <see cref="Box"/> class.
         /// </summary>
-        public override float Width
+        /// <param name="rectangle">A rectangle describing the offset and size of the box.</param>
+        public Box(RectangleF rectangle)
+            : base(true, rectangle)
         {
-            get { return size.Width; }
-            set { size.Width = value; }
+            this.hasFill = true;
+            this.fill = Color.White;
+
+            this.hasBorder = true;
+            this.border = Color.Black;
+
+            this.width = 0.5f;
         }
 
         /// <summary>
