707d697cb7de7a7159150eb19a3304009bf10b26
deaddog <mikkelslsl@gmail.com>
2015-11-16 19:57:33 +0100

    Implemented fill color in the FillObject class.

--- Objects/Leafs/FillObject.cs
+++ Objects/Leafs/FillObject.cs
@@ -7,10 +7,13 @@ namespace DeadDog.PDF
     /// </summary>
     public abstract class FillObject : StrokeObject
     {
+        private Color fill;
+        private bool hasFill;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="FillObject"/> class.
         /// </summary>
-        public FillObject(bool canResize) : base(canResize)
+        public FillObject(bool canResize) : this(canResize, RectangleF.Empty)
         {
         }
         /// <summary>
@@ -19,6 +22,25 @@ namespace DeadDog.PDF
         /// <param name="rectangle">A rectangle describing the offset and size of the object.</param>
         public FillObject(bool canResize, RectangleF rectangle) : base(canResize, rectangle)
         {
+            this.fill = Color.White;
+            this.hasFill = true;
+        }
+
+        /// <summary>
+        /// Gets or sets whether the filling of this object is displayed.
+        /// </summary>
+        public bool HasFill
+        {
+            get { return hasFill; }
+            set { hasFill = value; }
+        }
+        /// <summary>
+        /// Gets or sets the color used for filling this object.
+        /// </summary>
+        public Color FillColor
+        {
+            get { return fill; }
+            set { fill = value; hasFill = true; }
         }
     }
 }
