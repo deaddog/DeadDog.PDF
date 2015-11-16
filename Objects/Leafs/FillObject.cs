07479ca1e9b642c09737eb776bdfcc7369c7fd37
deaddog <mikkelslsl@gmail.com>
2015-11-16 19:56:01 +0100

    Created another base class for object with fill and stroke.

---
+++ Objects/Leafs/FillObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace DeadDog.PDF
+{
+    /// <summary>
+    /// Provides properties for managing objects with a border/stroke and fill color.
+    /// </summary>
+    public abstract class FillObject : StrokeObject
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FillObject"/> class.
+        /// </summary>
+        public FillObject(bool canResize) : base(canResize)
+        {
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FillObject"/> class.
+        /// </summary>
+        /// <param name="rectangle">A rectangle describing the offset and size of the object.</param>
+        public FillObject(bool canResize, RectangleF rectangle) : base(canResize, rectangle)
+        {
+        }
+    }
+}
