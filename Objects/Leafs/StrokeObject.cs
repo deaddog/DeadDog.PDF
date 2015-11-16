d70a4b53b6375dbfbb039b8a61220e919cb5d457
deaddog <mikkelslsl@gmail.com>
2015-11-16 19:47:38 +0100

    Created a base-class for objects that have a border.

---
+++ Objects/Leafs/StrokeObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace DeadDog.PDF
+{
+    public abstract class StrokeObject : PDFObject
+    {
+        public StrokeObject(bool canResize) : base(canResize)
+        {
+        }
+
+        public StrokeObject(bool canResize, RectangleF rectangle) : base(canResize, rectangle)
+        {
+        }
+    }
+}
