aad62c686e24fb54963162cb4041d2bb7cab45f7
deaddog <mikkelslsl@gmail.com>
2015-11-27 21:57:38 +0100

    An objects offset and size can be constrained (when they are set) using a type-defined method.

--- Objects/PDFObject.cs
+++ Objects/PDFObject.cs
@@ -6,7 +6,7 @@
     public abstract class PDFObject
     {
         private Vector2D offset, size;
-        
+
         /// <summary>
         /// Initializes a new instance of the <see cref="PDFObject"/> class.
         /// </summary>
@@ -14,8 +14,10 @@
         /// <param name="size">The size of the <see cref="PDFObject"/>.</param>
         public PDFObject(Vector2D offset, Vector2D size)
         {
-            this.offset = offset;
-            this.size = size;
+            this.offset = Vector2D.Zero;
+            this.size = Vector2D.Zero;
+
+            boundsChange(offset.X, offset.Y, size.X, size.Y);
         }
 
         /// <summary>
@@ -24,16 +26,16 @@
         public Vector2D Offset
         {
             get { return offset; }
-            set { offset = value; }
+            set { boundsChange(value.X, value.Y, size.X, size.Y); }
         }
-        
+
         /// <summary>
         /// Gets or sets the size of this <see cref="PDFObject"/>.
         /// </summary>
         public Vector2D Size
         {
             get { return getSize(); }
-            set { size = value; }
+            set { boundsChange(offset.X, offset.Y, value.X, value.Y); }
         }
 
         /// <summary>
@@ -43,6 +45,27 @@
         protected virtual Vector2D getSize()
         {
             return size;
+        }
+
+        private void boundsChange(Vector1D offsetX, Vector1D offsetY, Vector1D width, Vector1D height)
+        {
+            InnerBoundsChange(ref offsetX, ref offsetY, ref width, ref height);
+
+            this.offset = new Vector2D(offsetX, offsetY);
+            this.size = new Vector2D(width, height);
+        }
+
+        /// <summary>
+        /// When implemented in a derived class, ensures that the offset and size values specified for an object are "acceptable".
+        /// This method will be executed when changes are made to an objects size or offset, before the actual change is applied to the object.
+        /// When this method exits the object is updated with the parameter values.
+        /// </summary>
+        /// <param name="offsetX">The new x-coordinate of the objects local offset.</param>
+        /// <param name="offsetY">The new y-coordinate of the objects local offset.</param>
+        /// <param name="width">The new width of the object.</param>
+        /// <param name="height">The new height of the object.</param>
+        protected virtual void InnerBoundsChange(ref Vector1D offsetX, ref Vector1D offsetY, ref Vector1D width, ref Vector1D height)
+        {
         }
     }
 }
