a9a017cc78912d85b4b23f51a6f4689ad2578728
deaddog <mikkelslsl@gmail.com>
2015-11-27 09:01:09 +0100

    Added an Arc leaf. The size of this object does not correspond to its visual representation.

---
+++ Objects/Leafs/Arc.cs
@@ -0,0 +1,46 @@
+﻿using iTextSharp.text.pdf;
+using System;
+
+namespace DeadDog.PDF
+{
+    public class Arc : FillObject
+    {
+        private float startangle, extent;
+
+        public Arc()
+            : this(Vector2D.Zero)
+        {
+        }
+        public Arc(Vector2D size)
+            : base(true, Vector2D.Zero, size)
+        {
+            this.startangle = 0;
+            this.extent = 45;
+        }
+
+        public float StartAngle
+        {
+            get { return startangle; }
+            set { startangle = value; }
+        }
+        public float Extent
+        {
+            get { return extent; }
+            set { extent = value; }
+        }
+
+        protected internal override void Render(PdfContentByte cb, Vector2D offset)
+        {
+            var p1 = offset;
+            var p2 = offset + Size;
+
+            cb.Arc(
+                (float)p1.X.Value(UnitsOfMeasure.Points),
+                (float)p1.Y.Value(UnitsOfMeasure.Points),
+                (float)p2.X.Value(UnitsOfMeasure.Points),
+                (float)p2.Y.Value(UnitsOfMeasure.Points),
+                startangle,
+                extent);
+        }
+    }
+}
