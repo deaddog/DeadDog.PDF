28f34b944ddc2eb1d2f5a252e447867e8d58603d
deaddog <mikkelslsl@gmail.com>
2015-11-27 20:54:18 +0100

    The LeafObject supports rendering using the new ContentWriter.

--- Objects/Leafs/LeafObject.cs
+++ Objects/Leafs/LeafObject.cs
@@ -27,6 +27,25 @@ namespace DeadDog.PDF
         {
         }
 
-        internal protected abstract void Render(PdfContentByte cb, Vector2D offset);
+        /// <summary>
+        /// Renders the object in a document using a <see cref="ContentWriter"/>.
+        /// This method can be overridden in derived classes to specify rendering.
+        /// </summary>
+        /// <param name="cw">The <see cref="ContentWriter"/> used for rendering.</param>
+        /// <param name="offset">The calculated offset for the rendered item.</param>
+        protected virtual void Render(ContentWriter cw, Vector2D offset)
+        {
+        }
+        /// <summary>
+        /// Renders the object in a document using a <see cref="PdfContentByte"/> by invoking the <see cref="Render(ContentWriter, Vector2D)"/> method.
+        /// This method can be overridden in derived classes to specify rendering.
+        /// </summary>
+        /// <param name="cb">The <see cref="PdfContentByte"/> used for rendering.</param>
+        /// <param name="offset">The calculated offset for the rendered item.</param>
+        protected internal virtual void Render(PdfContentByte cb, Vector2D offset)
+        {
+            using (var writer = new ContentWriter(cb))
+                Render(writer, offset);
+        }
     }
 }
