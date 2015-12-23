188c49513f506f6a7431b7b1595a603f0ddce371
deaddog <mikkelslsl@gmail.com>
2015-12-23 08:29:43 +0100

    The EmptyBox is implemented as a LeafObject without speficying a Render method.

--- Objects/Leafs/EmptyBox.cs
+++ Objects/Leafs/EmptyBox.cs
@@ -3,7 +3,7 @@ namespace DeadDog.PDF
     /// <summary>
     /// Holds information on the size of a box. The box is not drawn but only used as a space buffer.
     /// </summary>
-    public sealed class EmptyBox : PDFObject
+    public sealed class EmptyBox : LeafObject
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="EmptyBox"/> class.
